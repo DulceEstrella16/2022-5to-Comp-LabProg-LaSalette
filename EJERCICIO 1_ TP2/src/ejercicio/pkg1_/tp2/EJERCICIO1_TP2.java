e9253237077233c134092487753a43e6e0d46561
DulceEstrella16 <101134392+DulceEstrella16@users.noreply.github.com>
2022-04-07 14:10:23 -0300

    Ejercicio 1 tp 2

---
+++ EJERCICIO 1_ TP2/src/ejercicio/pkg1_/tp2/EJERCICIO1_TP2.java
@@ -0,0 +1,12 @@
+package ejercicio.pkg1_.tp2;
+
+import java.util.Scanner;
+
+public class EJERCICIO1_TP2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String nombre; 
+        String apellido;
+        double salario_mensual;
+        
+}
