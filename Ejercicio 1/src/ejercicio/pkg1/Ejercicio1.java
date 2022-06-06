9008a5e4c8333cd5d4185b33fb63d587acbdc27a
DulceEstrella16 <101134392+DulceEstrella16@users.noreply.github.com>
2022-06-06 15:48:03 -0300

    TP4

---
+++ Ejercicio 1/src/ejercicio/pkg1/Ejercicio1.java
@@ -0,0 +1,24 @@
+package ejercicio.pkg1;
+public class Ejercicio1 {
+        public static void main(String[] args) {
+        Alumno alumno1 = new Alumno ("Mateo", "Garcia", 17, 7, 8, 7);
+        Alumno alumno2 = new Alumno ("Lautaro", "Castiglione", 17, 6, 8, 10);
+        Alumno alumno3 = new Alumno ("Lucciano", "Mancuso", 17, 5, 7, 8);
+        Alumno alumno4 = new Alumno ("Facundo", "Sequin", 17, 10, 7, 8);
+        Alumno alumno5 = new Alumno ("Franco", "Ciamarra",17, 6, 7, 5);
+        Alumno alumno6 = new Alumno ("Maximiliano", "Sosa", 17, 6, 7, 8);
+        Alumno alumno7 = new Alumno ("Ezequiel", "Diaz", 17, 8,6,7);
+        Alumno alumno8 = new Alumno ("Marcos", "Rodriguez", 17, 6, 7, 9);
+        Alumno alumno9 = new Alumno ("Napoleon", "Nan", 17, 8, 6, 5);
+        Alumno alumno10 = new Alumno ("Santiago", "Monges", 17, 6, 5, 7);
+        Alumno alumno11 = new Alumno ("Mateo", "Martin", 17, 6, 8, 9);
+        Alumno alumno12 = new Alumno ("Leo", "Da Silva", 17, 10, 10, 9);
+        Alumno alumno13 = new Alumno ("Matias", "Palacios", 17, 4, 8, 6);
+        Alumno alumno14 = new Alumno ("Juan","Curcio", 17, 6, 5, 6);
+        Alumno alumno15 = new Alumno ("Dulce","Carrizo", 17, 8, 7, 7);
+        
+    }
+        ArrayList
+    }
+    
+
